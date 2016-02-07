6bb6acacf7aca57fca21e5f1642b041a52a49a21
Luis Gonzalez <lgonzalez1415@gmail.com>
2016-02-07 12:54:48 -0800

    Skeleton code to build on

---
+++ project/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../project_ccs.cmd 
+
+C_SRCS += \
+../project.c \
+../startup_ccs.c 
+
+OBJS += \
+./project.obj \
+./startup_ccs.obj 
+
+C_DEPS += \
+./project.pp \
+./startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"project.pp" \
+"startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"project.obj" \
+"startup_ccs.obj" 
+
+C_SRCS__QUOTED += \
+"../project.c" \
+"../startup_ccs.c" 
+
+
